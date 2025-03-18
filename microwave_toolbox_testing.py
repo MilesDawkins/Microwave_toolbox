bb6d3158f036de28e80df22203cc09ec4f47383e
Miles Dawkins <milesdawkins0@gmail.com>
2025-03-18 00:14:09 -0600

    checked for run time, nearly 10 times as fast

--- microwave_toolbox_testing.py
+++ microwave_toolbox_testing.py
@@ -8,11 +8,11 @@ file = r"C:\Users\miles\Downloads\BFP840FESD_VCE_2.0V_IC_22mA.s2p"
 file = file.replace("\\", "/")
 trans_s2p = mt.network.s_param(file_path=file)
 
-file = r"C:\Users\miles\Downloads\BFP840_EDITED.s2p"
+file = r"C:\Users\miles\Downloads\BFP840FESD_VCE_2.0V_IC_22mA.s2p"
 file = file.replace("\\", "/")
 filter = mt.network.s_param(file_path=file)
 
-test = mt.network.s_param_cascade(trans_s2p,filter,interp_freq_step=10E6)
+test = mt.network.s_param_cascade(trans_s2p,filter,interp_freq_step=1E6)
 plot.plot(trans_s2p.frequencies,trans_s2p.phase[0][0])
 plot.grid()
 
