e5512a41edf73fbd3bc86ee586ed59961d0d8dbe
Miles Dawkins <milesdawkins0@gmail.com>
2025-04-05 22:55:00 -0600

    succesfully calcualtes gain circles, max gain paramewters, and transistor load and source impedances

--- microwave_toolbox/circuit_tools.py
+++ microwave_toolbox/circuit_tools.py
@@ -16,3 +16,31 @@ class rf_amplifier():
         self.frequencies = s2p_in.frequencies
         self.g_s_max_gain = [1/(1-abs(x)**2) for x in self.transistor.complex[0][0]]
         self.gamma_s_max_gain = [np.conjugate(x) for x in self.transistor.complex[0][0]]
+        self.g_l_max_gain = [1/(1-abs(x)**2) for x in self.transistor.complex[1][1]]
+        self.gamma_l_max_gain = [np.conjugate(x) for x in self.transistor.complex[1][1]]
+        self.max_z0_transducer_gain = [abs(x)**2 for x in self.transistor.complex[1][0]]
+        self.max_transducer_gain = [x + y + z for x,y,z in zip(self.g_l_max_gain,self.g_s_max_gain,self.max_z0_transducer_gain)]
+    
+    def calc_gain_circle(self,log_gain,freq):
+        gain = 10**(log_gain/10)
+        g_s_max = np.interp(freq,self.frequencies,self.g_s_max_gain)
+        g_s_norm = gain/g_s_max
+        g_l_max = np.interp(freq,self.frequencies,self.g_l_max_gain)
+        g_l_norm = gain/g_l_max
+        s11 = np.interp(freq,self.frequencies,self.transistor.complex[0][0])
+        s22 = np.interp(freq,self.frequencies,self.transistor.complex[1][1])
+
+        source_center = (g_s_norm*np.conjugate(s11))/(1-np.abs(s11)**2*(1-g_s_norm))
+        load_center = (g_l_norm*np.conjugate(s22))/(1-np.abs(s22)**2*(1-g_l_norm))
+
+        source_radius = (np.sqrt(1-g_s_norm)*(1-np.abs(s11)**2))/(1-np.abs(s11)**2*(1-g_s_norm))
+        load_radius = (np.sqrt(1-g_l_norm)*(1-np.abs(s22)**2))/(1-np.abs(s22)**2*(1-g_l_norm))
+
+        return source_center,source_radius,load_center,load_radius
+    
+    def calc_transducer_impedance(self,freq):
+        gamma_s = np.interp(freq,self.frequencies,self.transistor.complex[0][0])
+        source_imp = 50*((-1-gamma_s)/(gamma_s-1))
+        gamma_l = np.interp(freq,self.frequencies,self.transistor.complex[1][1])
+        load_imp = 50*((-1-gamma_l)/(gamma_l-1))
+        return source_imp,load_imp
--- microwave_toolbox_testing.py
+++ microwave_toolbox_testing.py
@@ -1,5 +1,6 @@
 import microwave_toolbox as mt
 import matplotlib.pyplot as plot
+import matplotlib.patches as patches
 import numpy as np
 import os
 import time
@@ -12,17 +13,33 @@ print(mt.antenna_tools.fspl_calc(8E9,8,dist_units = "feet"))
 trans_s2p = mt.system_tools.network(file_path=file)
 #mt.system_tools.reverse_network(trans_s2p)
 amp = mt.circuit_tools.rf_amplifier(trans_s2p)
-ax = mt.plotting_tools.smith_chart.__init__()
-real = [np.real(x) for x in amp.gamma_s_max_gain]
-imag = [np.imag(x) for x in amp.gamma_s_max_gain]
-ax.plot(real,imag)
-plot.grid()
 
+smith = mt.plotting_tools.smith_chart()
+real = [np.real(x) for x in amp.transistor.complex[1][1]]
+imag = [np.imag(x) for x in amp.transistor.complex[1][1]]
+smith.ax.plot(real,imag)
+plot.grid()
+#annotate every 1GHz
 point = 0
 for xy in zip(real, imag):  
     if amp.frequencies[point]%1E9 == 0:
-        ax.annotate('(%s)' % (amp.frequencies[point]/1E9), xy=xy, textcoords='data') 
+        smith.ax.annotate('(%s)' % (amp.frequencies[point]/1E9), xy=xy, textcoords='data') 
     point = point + 1
+
+source,load = amp.calc_transducer_impedance(4.01E9)
+print(load)
+print(10*np.log10(np.interp(4.1E9,amp.frequencies,amp.max_z0_transducer_gain)))
+
+sc,sr,lc,lr = amp.calc_gain_circle(0.25,3E9)
+xl = np.real(lc)
+yl = np.imag(lc)
+xs = np.real(sc)
+ys = np.imag(sc)
+smith.ax.add_patch(patches.Circle((xl,yl),lr, edgecolor='r', facecolor='none'))
+smith.ax.add_patch(patches.Circle((xs,ys),sr, edgecolor='r', facecolor='none'))
+
+
+
 print("--- %s seconds ---" % (time.time() - start_time))
 plot.show()
 
