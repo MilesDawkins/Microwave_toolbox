2f33ddaada3fd46d400aeecda2f3fb544971b955
Miles Dawkins <milesdawkins0@gmail.com>
2025-04-05 21:13:38 -0600

    amp class calculates max gaim reflection coefficient and max source gain

--- microwave_toolbox/circuit_tools.py
+++ microwave_toolbox/circuit_tools.py
@@ -5,12 +5,14 @@ from . import system_tools as st
 from . import plotting_tools
 
 
-class amplifier():
+class rf_amplifier():
 
-    def __init__(self, s2p: st.network):
+    def __init__(self, s2p_in: st.network):
+        #initialize basic variables
         self.type = "Amplifier"
         self.sub_type = "None"
         self.z_reference = 50
-        self.reversed = False
-        
-    
+        self.transistor = s2p_in
+        self.frequencies = s2p_in.frequencies
+        self.g_s_max_gain = [1/(1-abs(x)**2) for x in self.transistor.complex[0][0]]
+        self.gamma_s_max_gain = [np.conjugate(x) for x in self.transistor.complex[0][0]]
--- microwave_toolbox_testing.py
+++ microwave_toolbox_testing.py
@@ -1,9 +1,8 @@
 import microwave_toolbox as mt
 import matplotlib.pyplot as plot
 import numpy as np
-import os, sys
+import os
 import time
-
 
 start_time = time.time()
 
@@ -12,16 +11,18 @@ file = os.path.join(script_directory,"BFP840FESD_VCE_2.0V_IC_22mA.s2p")
 print(mt.antenna_tools.fspl_calc(8E9,8,dist_units = "feet"))
 trans_s2p = mt.system_tools.network(file_path=file)
 #mt.system_tools.reverse_network(trans_s2p)
-cascade = mt.system_tools.network_cascade(trans_s2p,trans_s2p,10E6)
-
-plot.plot(cascade.frequencies,cascade.dbmag[0][1])
+amp = mt.circuit_tools.rf_amplifier(trans_s2p)
+ax = mt.plotting_tools.smith_chart.__init__()
+real = [np.real(x) for x in amp.gamma_s_max_gain]
+imag = [np.imag(x) for x in amp.gamma_s_max_gain]
+ax.plot(real,imag)
 plot.grid()
-"""smith chart testing
-ax=mt.plot.smith_chart_matplotlib.smith_chart.__init__()
-ax.plot([np.real(x) for x in cascade.complex[1][1]],[np.imag(y) for y in cascade.complex[1][1]])
-ax.grid()
-ax.set_aspect('equal')
-"""
 
+point = 0
+for xy in zip(real, imag):  
+    if amp.frequencies[point]%1E9 == 0:
+        ax.annotate('(%s)' % (amp.frequencies[point]/1E9), xy=xy, textcoords='data') 
+    point = point + 1
 print("--- %s seconds ---" % (time.time() - start_time))
 plot.show()
+
