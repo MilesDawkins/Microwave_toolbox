dfa4a9646efba18bf32205e7c5141e282fa7529c
Miles Dawkins <milesdawkins0@gmail.com>
2025-04-06 17:03:53 -0600

    changed testing toolbox to calculate source and load network for specific gain

--- microwave_toolbox_testing.py
+++ microwave_toolbox_testing.py
@@ -12,18 +12,21 @@ file = os.path.join(script_directory,"BFP840FESD_VCE_2.0V_IC_22mA.s2p")
 
 #################calulation functions###########################
 freq = 4E9
+desired_gain = 20
 trans_s2p = mt.system_tools.network(file_path=file)
 amp = mt.circuit_tools.rf_amplifier(trans_s2p)
 source,load = amp.calc_transducer_impedance(freq)
-sc,sr,lc,lr,min_gs,min_gl = amp.calc_gain_circle(0.4,freq)
+trans_gain = 10*np.log10(np.interp(freq,amp.frequencies,amp.max_z0_transducer_gain))
+sl_gain_needed = (desired_gain - trans_gain)/2
+sc,sr,lc,lr,min_gs,min_gl = amp.calc_gain_circle(sl_gain_needed,freq)
 xl,yl = np.real(lc),np.imag(lc)
 xs,ys = np.real(sc),np.imag(sc)
-print(10*np.log10(np.interp(freq,amp.frequencies,amp.max_z0_transducer_gain)))
+print(trans_gain)
 print(10*np.log10(np.interp(freq,amp.frequencies,amp.g_s_max_gain)))
 print(10*np.log10(np.interp(freq,amp.frequencies,amp.g_l_max_gain)))
 print(source*((-1-min_gs)/(min_gs-1)))
 print(load*((-1-min_gl)/(min_gl-1)))
-
+print(trans_gain+2*sl_gain_needed)
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ##################plotting functions#######################
