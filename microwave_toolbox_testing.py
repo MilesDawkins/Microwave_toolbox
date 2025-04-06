518fc184057797ad255561886abd4e34c4c6cac3
Miles Dawkins <milesdawkins0@gmail.com>
2025-04-06 16:35:03 -0600

    cleaned up testing script, working on calculations for matching network of amplifier

--- microwave_toolbox_testing.py
+++ microwave_toolbox_testing.py
@@ -5,45 +5,43 @@ import numpy as np
 import os
 import time
 
+##############setup functions####################
 start_time = time.time()
-
 script_directory = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join(script_directory,"BFP840FESD_VCE_2.0V_IC_22mA.s2p")
-print(mt.antenna_tools.fspl_calc(8E9,8,dist_units = "feet"))
-trans_s2p = mt.system_tools.network(file_path=file)
-casc=mt.system_tools.network_cascade(trans_s2p,trans_s2p)
-#mt.system_tools.reverse_network(trans_s2p)
-amp = mt.circuit_tools.rf_amplifier(casc)
 
+#################calulation functions###########################
+trans_s2p = mt.system_tools.network(file_path=file)
+amp = mt.circuit_tools.rf_amplifier(trans_s2p)
+source,load = amp.calc_transducer_impedance(4E9)
+sc,sr,lc,lr,min_gs,min_gl = amp.calc_gain_circle(-12.53/2,4E9)
+xl,yl = np.real(lc),np.imag(lc)
+xs,ys = np.real(sc),np.imag(sc)
+print(10*np.log10(np.interp(4E9,amp.frequencies,amp.max_z0_transducer_gain)))
+print(10*np.log10(np.interp(4E9,amp.frequencies,amp.g_s_max_gain)))
+print(10*np.log10(np.interp(4E9,amp.frequencies,amp.g_l_max_gain)))
+print(source*((-1-min_gs)/(min_gs-1)))
+print(load*((-1-min_gl)/(min_gl-1)))
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+##################plotting functions#######################
 smith = mt.plotting_tools.smith_chart()
 real = [np.real(x) for x in amp.transistor.complex[1][1]]
 imag = [np.imag(x) for x in amp.transistor.complex[1][1]]
 smith.ax.plot(real,imag)
-plot.grid()
-#annotate every 1GHz
+    #annotate every 1GHz
 point = 0
 for xy in zip(real, imag):  
     if amp.frequencies[point]%1E9 == 0:
         smith.ax.annotate('(%s)' % (amp.frequencies[point]/1E9), xy=xy, textcoords='data') 
+        smith.ax.plot(real[point],imag[point], marker='o',markersize = 2 , c='black', linestyle='none')
     point = point + 1
-
-source,load = amp.calc_transducer_impedance(4E9)
-
-sc,sr,lc,lr,min_gs,min_gl = amp.calc_gain_circle(0.1,1E9)
-xl = np.real(lc)
-yl = np.imag(lc)
-xs = np.real(sc)
-ys = np.imag(sc)
-#smith.ax.add_patch(patches.Circle((xl,yl),lr, edgecolor='r', facecolor='none'))
 smith.ax.add_patch(patches.Circle((xs,ys),sr, edgecolor='r', facecolor='none'))
 smith.ax.add_patch(patches.Circle((xl,yl),lr, edgecolor='r', facecolor='none'))
-print(source*((-1-min_gs)/(min_gs-1)))
-print(load*((-1-min_gl)/(min_gl-1)))
 smith.ax.plot(np.real(min_gs),np.imag(min_gs), marker='o', linestyle='none')
 smith.ax.plot(np.real(min_gl),np.imag(min_gl), marker='o', linestyle='none')
-
-
-print("--- %s seconds ---" % (time.time() - start_time))
+plot.grid()
 plot.show()
 
 
