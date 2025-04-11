5809960126f748f200a03ff6fea26fec58ffef63
Miles Dawkins <milesdawkins0@gmail.com>
2025-04-11 15:23:39 -0600

    amplifier code is good for now, need to figure out how to cascade t_lines

--- microwave_toolbox_testing.py
+++ microwave_toolbox_testing.py
@@ -15,9 +15,12 @@ freq = 10E9
 desired_gain = 15
 
 trans_s2p = mt.system_tools.network(file_path=file)
-t_line = mt.t_line_tools.microstrip(100,4.4,1.6E-3)
-t_line.create_network(trans_s2p.frequencies,0.12)
-amp_w_line = mt.system_tools.network_cascade(t_line.network,trans_s2p)
+t_line = mt.t_line_tools.microstrip(50,4.4,1.6E-3, type = "open")
+t_line.create_network(trans_s2p.frequencies,0.005)
+t_line_2 = mt.t_line_tools.microstrip(50,4.4,1.6E-3)
+t_line_2.create_network(trans_s2p.frequencies,0.1)
+amp_w_line = mt.system_tools.network_cascade(t_line_2.network,trans_s2p)
+t_cascade = mt.system_tools.network_cascade(t_line_2.network,t_line.network)
 amp = mt.circuit_tools.rf_amplifier(amp_w_line)
 
 source,load = amp.calc_transducer_impedance(freq)
@@ -30,8 +33,6 @@ xs,ys = np.real(sc),np.imag(sc)
 print(trans_gain)
 print(10*np.log10(np.interp(freq,amp.frequencies,amp.g_s_max_gain)))
 print(10*np.log10(np.interp(freq,amp.frequencies,amp.g_l_max_gain)))
-print((50*((-1-min_gs)/(min_gs-1))))
-print((50*((-1-min_gl)/(min_gl-1))))
 print(trans_gain+2*sl_gain_needed)
 print("--- %s seconds ---" % (time.time() - start_time))
 
@@ -40,20 +41,22 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 ##################plotting functions#######################
 smith = mt.plotting_tools.smith_chart()
-real = [np.real(x) for x in amp.transistor.complex[1][1]]
-imag = [np.imag(x) for x in amp.transistor.complex[1][1]]
-real2 = [np.real(x) for x in t_line.network.complex[0][0]]
-imag2 = [np.imag(x) for x in t_line.network.complex[0][0]]
-smith.ax.plot(real,imag)
+real = [np.real(x) for x in amp_w_line.complex[0][0]]
+imag = [np.imag(x) for x in amp_w_line.complex[0][0]]
+real2 = [np.real(x) for x in t_cascade.complex[0][0]]
+imag2 = [np.imag(x) for x in t_cascade.complex[0][0]]
+#smith.ax.plot(real,imag)
 smith.ax.plot(real2,imag2)
-smith.ax.plot(real2[1],imag2[1], marker='o',markersize = 2 ,  linestyle='none')
+
+"""
     #annotate every 1GHz
 point = 0
 for xy in zip(real, imag):  
     if amp.frequencies[point]%1E9 == 0:
         smith.ax.annotate('(%s)' % (amp.frequencies[point]/1E9), xy=xy, textcoords='data') 
-        smith.ax.plot(real[point],imag[point], marker='o',markersize = 2 , c='black', linestyle='none')
+        smith.ax.plot(real2[point],imag2[point], marker='o',markersize = 2 , c='black', linestyle='none')
     point = point + 1
+"""
 smith.ax.add_patch(patches.Circle((xs,ys),sr, edgecolor='r', facecolor='none'))
 smith.ax.add_patch(patches.Circle((xl,yl),lr, edgecolor='r', facecolor='none'))
 smith.ax.plot(np.real(min_gs),np.imag(min_gs), marker='o', linestyle='none')
@@ -63,7 +66,7 @@ plot.grid()
 
 
 fig2, ay = plot.subplots()
-ay.plot(t_line.network.frequencies,t_line.network.dbmag[0][0])
+ay.plot(t_cascade.frequencies,t_cascade.dbmag[0][0])
 plot.grid()
 plot.show()
 
