85d314e759d2ca8949734b3946ebf2be052ecbb2
Miles Dawkins <milesdawkins0@gmail.com>
2025-04-24 18:30:58 -0600

    first light of amplifier cascade with shunt t-line. issues with shorted shunt t-line are still there

--- microwave_toolbox_testing.py
+++ microwave_toolbox_testing.py
@@ -11,26 +11,29 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join(script_directory,"BFP840FESD_VCE_2.0V_IC_22mA.s2p")
 
 #################calulation functions###########################
-freqs = np.linspace(1E9,10E9,1000)
+freqs = np.linspace(1E9,11E9,1000)
 
-t_line_s = mt.t_line_tools.microstrip(25,1,1.6E-3,37.5E-3,freqs_in = freqs, typem = "short", shunt_in=True)
-t_line = mt.t_line_tools.microstrip(100,1,1.6E-3,20E-3,freqs_in = freqs)
+bjt = mt.system_tools.network(file)
+
+t_line_s = mt.t_line_tools.microstrip(50,1,1.6E-3,10E-3,freqs_in = freqs, typem= "loaded", zl_in=1000, shunt_in=True)
+t_line = mt.t_line_tools.microstrip(50,1,1.6E-3,20E-3,freqs_in = freqs)
 
 shunt_t_line_cascade = mt.system_tools.network_cascade(t_line.network,t_line_s.network)
 shunt_t_line_cascade = mt.system_tools.network_cascade(shunt_t_line_cascade,t_line.network)
-
+amp = mt.system_tools.network_cascade(bjt,shunt_t_line_cascade)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
 
 ##################plotting functions#######################
-plot.plot(shunt_t_line_cascade.frequencies,shunt_t_line_cascade.dbmag[0][0])
-plot.ylim([-40,10])
+plot.plot(amp.frequencies,amp.dbmag[1][0])
+
 
 smith = mt.plotting_tools.smith_chart()
 real = [np.real(x) for x in shunt_t_line_cascade.complex[0][0]]
 imag = [np.imag(x) for x in shunt_t_line_cascade.complex[0][0]]
 smith.ax.plot(real,imag)
+"""
 #annotate every 1GHz
 point = 0
 for xy in zip(real, imag):  
@@ -38,6 +41,7 @@ for xy in zip(real, imag):
         smith.ax.annotate('(%s)' % (t_line.network.frequencies[point]/1E9), xy=xy, textcoords='data') 
         smith.ax.plot(real[point],imag[point], marker='o',markersize = 2 , c='black', linestyle='none')
     point = point + 1
+"""
 plot.grid()
 plot.show()
 
