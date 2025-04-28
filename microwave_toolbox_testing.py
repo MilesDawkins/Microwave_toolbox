eafd54b9f50f9eadded8692c5db81ea7f8a6094f
Miles Dawkins <milesdawkins0@gmail.com>
2025-04-28 08:46:25 -0600

    first potential working design?

--- microwave_toolbox_testing.py
+++ microwave_toolbox_testing.py
@@ -11,41 +11,27 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join(script_directory,"BFP840FESD_VCE_2.0V_IC_22mA.s2p")
 
 #################calulation functions###########################
-freqs = np.linspace(1, 10E9,1000)
+freqs = np.linspace(10E9, 10.01E9,1000)
 bjt = mt.system_tools.network(file)
 
-f0 = 1.5E9
-sl = 0.25
-pl = 0.25
-
-z1 = 50
-z2 = 39#30.1
-z12 = 48.4
-z23 = 49.1
-
-
-shunt_match1 = mt.t_line_tools.microstrip(z1,4.4,1.6E-3,sl,freqs_in = freqs, typem="short", shunt_in=True, length_unit="lambda", center_freq = f0)
-shunt_match2 = mt.t_line_tools.microstrip(z2,4.4,1.6E-3,sl,freqs_in = freqs, typem="short", shunt_in=True, length_unit="lambda", center_freq = f0)
-
-phase_match1 = mt.t_line_tools.microstrip(z12,4.4,1.6E-3,pl,freqs_in = freqs, length_unit="lambda", center_freq= f0)
-phase_match2 = mt.t_line_tools.microstrip(z12,4.4,1.6E-3,pl,freqs_in = freqs, length_unit="lambda", center_freq= f0)
-
-network = shunt_match1.network ** phase_match1.network ** shunt_match2.network ** phase_match2.network ** shunt_match2.network ** phase_match1.network ** shunt_match1.network
+f0 = 10E9
+sl = 0.125
+pl = 0.13
+shunt_match1 = mt.t_line_tools.microstrip(50,4.4,1.6E-3,sl,freqs_in = freqs, typem="open", shunt_in=True, length_unit="lambda", center_freq = f0)
+phase_match1 = mt.t_line_tools.microstrip(50,4.4,1.6E-3,pl,freqs_in = freqs, length_unit="lambda", center_freq= f0)
+phase_match2 = mt.t_line_tools.microstrip(50,4.4,1.6E-3,0.001,freqs_in = freqs, typem= "loaded", zl_in = 50)
+network = phase_match1.network ** shunt_match1.network ** phase_match2.network
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ##################plotting functions#######################
-plot.plot(network.frequencies,network.dbmag[1][0])
-plot.plot(network.frequencies,network.dbmag[0][0])
-plot.ylim([-50,0])
 
-
-"""
 smith = mt.plotting_tools.smith_chart()
-real = [np.real(x) for x in shunt_t_line_cascade.complex[0][0]]
-imag = [np.imag(x) for x in shunt_t_line_cascade.complex[0][0]]
+real = [np.real(x) for x in network.complex]
+imag = [np.imag(x) for x in network.complex]
 smith.ax.plot(real,imag)
 
+"""
 #annotate every 1GHz
 point = 0
 for xy in zip(real, imag):  
