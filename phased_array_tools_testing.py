9fa20b88ead21924c8ae4113e9dce9c62bdc8629
Miles Dawkins <milesdawkins0@gmail.com>
2025-04-30 17:12:55 -0600

    succesfully simulting phased array with beam steer

--- phased_array_tools_testing.py
+++ phased_array_tools_testing.py
@@ -11,25 +11,31 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join(script_directory,"BFP840FESD_VCE_2.0V_IC_22mA.s2p")
 
 #################calulation functions###########################
-dpp = mt.antenna_tools.create_dipole(2E9,1000)
-num_ele = 2
+step_size = 360
+dpp = mt.antenna_tools.create_dipole(2E9,step_size)
+num_ele = 10
 x_spacing = (3E8/2E9)/2
+delta_phi = np.radians(20)
 
 ele_pos = np.zeros((num_ele,3))
 start_x = -(num_ele-1)*x_spacing/2
-#weights = [0.357,0.485,0.706,0.890,1,1,0.890,0.706, 0.485,0.357]
+weights = [0.357,0.485,0.706,0.890,1,1,0.890,0.706, 0.485,0.357]
+phases = np.zeros(10)
+for ele in range (num_ele):
+    phases[ele] = ele*delta_phi
+
 for x in range(num_ele):
     ele_pos[x][2] = start_x+x*x_spacing
-array = mt.phased_array_tools.element_array(dpp,ele_pos,1000)
-array.calc_array_factor(2E9,1000)
+array = mt.phased_array_tools.element_array(dpp,ele_pos,step_size, weights = weights, phases= phases)
+array.calc_array_factor(2E9,step_size)
 
-au = np.zeros((1000,500))
+au = np.zeros((step_size,int(step_size/2)))
 
-for p in range(1000):
-    au[p] = [10*np.log10(x * y) for x,y in zip(array.array_factor[p],dpp.rad_intensity[p])]
+for p in range(step_size):
+    au[p] = [20*np.log10(x * y) for x,y in zip(array.array_factor[p],dpp.rad_intensity[p])]
     
-phi = np.linspace(0,2*np.pi,1000)
-theta = np.linspace(0,np.pi,500)
+phi = np.linspace(0,2*np.pi,step_size)
+theta = np.linspace(0,np.pi,int(step_size/2))
 
 print("--- %s seconds ---" % (time.time() - start_time))
 ##################plotting functions#######################
@@ -48,7 +54,7 @@ plot.show()
 
 """
 fig, ax = plot.subplots(subplot_kw={'projection': 'polar'})
-ax.plot(phi,[au[x][int(1000/4)] for x in range(len(au))])
+ax.plot(phi,[au[x][int(step_size/4)] for x in range(len(au))])
 ax.set_rlim(-20,40)
 plot.show()
 
