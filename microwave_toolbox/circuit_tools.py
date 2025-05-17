c281a57fa67f0f8500bbdfc083d369b3533bd1ce
Miles Dawkins <milesdawkins0@gmail.com>
2025-05-16 21:07:49 -0600

    reworking amplifier design calculation scripts before moving onto antenna design revisit, will switch to new branch

--- microwave_toolbox/circuit_tools.py
+++ microwave_toolbox/circuit_tools.py
@@ -22,41 +22,44 @@ class rf_amplifier():
         self.z_reference = 50
         self.transistor = s2p_in
         self.frequencies = s2p_in.frequencies
-        self.g_s_max_gain = [1/(1-abs(x)**2) for x in self.transistor.complex[0][0]]
-        self.gamma_s_max_gain = [np.conjugate(x) for x in self.transistor.complex[0][0]]
-        self.g_l_max_gain = [1/(1-abs(x)**2) for x in self.transistor.complex[1][1]]
-        self.gamma_l_max_gain = [np.conjugate(x) for x in self.transistor.complex[1][1]]
-        self.max_z0_transducer_gain = [abs(x)**2 for x in self.transistor.complex[1][0]]
-        self.max_transducer_gain = [x + y + z for x,y,z in zip(self.g_l_max_gain,self.g_s_max_gain,self.max_z0_transducer_gain)]
+
+        self.source_max_gain = 1/(1-abs(self.transistor.complex[0,0])**2)
+        self.source_max_gain_gamma = np.conjugate(self.transistor.complex[0,0])
+
+        self.load_max_gain = 1/(1-abs(self.transistor.complex[1,1])**2)
+        self.load_max_gain_gamma = np.conjugate(self.transistor.complex[1,1])
+
+        self.max_z0_transducer_gain = abs(self.transistor.complex[1,0])**2
+
+        self.max_transducer_gain = self.load_max_gain*self.source_max_gain*self.max_z0_transducer_gain
     
-    def calc_gain_circle(self,log_gain,freq):
+    def calc_gain_circle(self,source_or_load: str,log_gain,freq):
         gain = 10**(log_gain/10)
-        g_s_max = np.interp(freq,self.frequencies,self.g_s_max_gain)
+        g_s_max = np.interp(freq,self.frequencies,self.source_max_gain)
         g_s_norm = gain/g_s_max
-        g_l_max = np.interp(freq,self.frequencies,self.g_l_max_gain)
+        g_l_max = np.interp(freq,self.frequencies,self.load_max_gain)
         g_l_norm = gain/g_l_max
-        s11 = np.interp(freq,self.frequencies,self.transistor.complex[0][0])
-        s22 = np.interp(freq,self.frequencies,self.transistor.complex[1][1])
+        s11 = np.interp(freq,self.frequencies,self.transistor.complex[0,0])
+        s22 = np.interp(freq,self.frequencies,self.transistor.complex[1,1])
 
-        source_center = (g_s_norm*np.conjugate(s11))/(1-np.abs(s11)**2*(1-g_s_norm))
-        load_center = (g_l_norm*np.conjugate(s22))/(1-np.abs(s22)**2*(1-g_l_norm))
+        if source_or_load == "source":
+            center = (g_s_norm*np.conjugate(s11))/(1-np.abs(s11)**2*(1-g_s_norm))
+            radius = (np.sqrt(1-g_s_norm)*(1-np.abs(s11)**2))/(1-np.abs(s11)**2*(1-g_s_norm))
+        
+        if source_or_load == "load":
+            center = (g_l_norm*np.conjugate(s22))/(1-np.abs(s22)**2*(1-g_l_norm))
+            radius = (np.sqrt(1-g_l_norm)*(1-np.abs(s22)**2))/(1-np.abs(s22)**2*(1-g_l_norm))
 
-        source_radius = (np.sqrt(1-g_s_norm)*(1-np.abs(s11)**2))/(1-np.abs(s11)**2*(1-g_s_norm))
-        load_radius = (np.sqrt(1-g_l_norm)*(1-np.abs(s22)**2))/(1-np.abs(s22)**2*(1-g_l_norm))
+        min_gamma_mag = np.abs(center)-np.abs(radius)
+        min_gamma_ang = np.atan2(np.imag(center),np.real(center))
+        min_gamma = (min_gamma_mag*np.cos(min_gamma_ang) + 1j*min_gamma_mag*np.sin(min_gamma_ang))
 
-        min_gamma_mag_s = np.abs(source_center)-np.abs(source_radius)
-        min_gamma_ang_s = np.atan2(np.imag(source_center),np.real(source_center))
-        min_gamma_source = (min_gamma_mag_s*np.cos(min_gamma_ang_s) + 1j*min_gamma_mag_s*np.sin(min_gamma_ang_s))
+        return center,radius,min_gamma
 
-        min_gamma_mag_l = np.abs(load_center)-np.abs(load_radius)
-        min_gamma_ang_l = np.atan2(np.imag(load_center),np.real(load_center))
-        min_gamma_load = (min_gamma_mag_l*np.cos(min_gamma_ang_l) + 1j*min_gamma_mag_l*np.sin(min_gamma_ang_l))
-
-        return source_center,source_radius,load_center,load_radius, min_gamma_source, min_gamma_load
     
     def calc_transducer_impedance(self,freq):
-        gamma_s = np.interp(freq,self.frequencies,self.transistor.complex[0][0])
+        gamma_s = np.interp(freq,self.frequencies,self.transistor.complex[0,0])
         source_imp = 50*((-1-gamma_s)/(gamma_s-1))
-        gamma_l = np.interp(freq,self.frequencies,self.transistor.complex[1][1])
+        gamma_l = np.interp(freq,self.frequencies,self.transistor.complex[1,1])
         load_imp = 50*((-1-gamma_l)/(gamma_l-1))
         return source_imp,load_imp
--- rf_amp_design_testing.py
+++ rf_amp_design_testing.py
@@ -12,75 +12,65 @@ file = os.path.join(script_directory,"BFP840FESD_VCE_2.0V_IC_22mA.s2p")
 
 #################calulation functions###########################
 freqs = np.linspace(1E1,12E9,1000)
-gain_req = 10
+gain_req = 16.78724978143363
 
 bjt = mt.system_tools.network(file)
 
 amp_calc = mt.circuit_tools.rf_amplifier(bjt)
 
 trans_gain = 10*np.log10(np.interp(10E9,amp_calc.frequencies,amp_calc.max_transducer_gain))
-match_gain = gain_req - trans_gain
-sc,sr,lc,lr, min_gs, min_gl = amp_calc.calc_gain_circle(match_gain/2,10E9)
-totalgain = trans_gain + match_gain
 print(trans_gain)
-print(totalgain)
-print(mt.system_tools.gamma_2_impedance(50,min_gs)/50)
-print(mt.system_tools.gamma_2_impedance(50,min_gl)/50)
+
+source_max_gain = 10*np.log10(np.interp(10E9,amp_calc.frequencies,amp_calc.source_max_gain))
+print(source_max_gain)
+
+sc,sr,min_gs= amp_calc.calc_gain_circle("source",source_max_gain-0.05,10E9)
+#lc,lr,min_gl= amp_calc.calc_gain_circle("load",match_gain/2,10E9)
+
+
 
 
 microstrip_ref = mt.t_line_tools.microstrip(50,4.4,1.6E-3,1)
 lamb = microstrip_ref.wavelength(10E9)
 
 gs_stub_l = (1/np.atan(mt.system_tools.gamma_2_impedance(50,np.abs(min_gs))/50))/(2*np.pi) # calculating required open circuit stub length
-gl_stub_l = (1/np.atan(mt.system_tools.gamma_2_impedance(50,np.abs(min_gl))/50))/(2*np.pi) # calculating required open circuit stub length
-
-gs_phase_l = (np.angle(min_gs)-np.angle(1/((1/50)+(1/(-1J*50*1/np.tan(2*np.pi*gs_stub_l))))))/(2*np.pi) #calculating phasing line length using differences of phase
-gl_phase_l = (np.angle(min_gl)-np.angle(1/((1/50)+(1/(-1J*50*1/np.tan(2*np.pi*gl_stub_l))))))/(2*np.pi) #calculating phasing line length using differences of phase
-
-print(gs_phase_l)
-print(gl_phase_l)
-print(gs_stub_l)
-print(gl_stub_l) 
+gs_phase_l = (np.angle(1/((1/50)+(1/(-1J*50*1/np.tan(2*np.pi*gs_stub_l)))))-np.angle(min_gs))/(2*np.pi) #calculating phasing line length using differences of phase
 
 
-phase_source =  mt.t_line_tools.microstrip(50,4.4,1.6E-3,gs_phase_l*lamb,freqs_in = freqs)
-shunt_source = mt.t_line_tools.microstrip(50,4.4,1.6E-3,gs_stub_l*lamb,freqs_in = freqs, typem="open", shunt_in=True)
 
-phase_load =  mt.t_line_tools.microstrip(50,4.4,1.6E-3,gl_phase_l*lamb,freqs_in = freqs)
-shunt_load = mt.t_line_tools.microstrip(50,4.4,1.6E-3,gl_stub_l*lamb,freqs_in = freqs, typem="open", shunt_in=True)
 
-source_match = mt.system_tools.network_cascade(shunt_source.network,phase_source.network)
-load_match = mt.system_tools.network_cascade(phase_load.network,shunt_load.network)
 
-amp_total =  shunt_source.network ** phase_source.network **  bjt ** phase_load.network ** shunt_load.network
+
+
+
+
+
+
+
+
+
+
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
-
 ##################plotting functions#######################
-plot.plot(amp_total.frequencies,amp_total.dbmag[1,0])
-plot.plot(bjt.frequencies,bjt.dbmag[1,0])
-#plot.plot(amp_total.frequencies,amp_total.dbmag[1,0])
-
-
-
 smith = mt.plotting_tools.smith_chart()
-zl = 50-1J*50*1/np.tan(2*np.pi*gl_stub_l)
-gam = ((zl-50)/(zl+50))
-print(gam)
-real = np.real(gam)
-imag = np.imag(gam)
-smith.ax.plot(real,imag,marker='o',markersize = 2 , c='black', linestyle='none')
 
-"""
+real = np.real(amp_calc.source_max_gain_gamma)
+imag = np.imag(amp_calc.source_max_gain_gamma)
+smith.ax.plot(real,imag)
+
+
+smith.ax.add_patch(patches.Circle((np.real(sc),np.imag(sc)), sr, edgecolor='r', facecolor='none'))
+
+
 #annotate every 1GHz
 point = 0
 for xy in zip(real, imag):  
-    if t_line.network.frequencies[point]%1E9 == 0:
-        smith.ax.annotate('(%s)' % (t_line.network.frequencies[point]/1E9), xy=xy, textcoords='data') 
+    if bjt.frequencies[point]%1E9 == 0:
+        smith.ax.annotate('(%s)' % (bjt.frequencies[point]/1E9), xy=xy, textcoords='data') 
         smith.ax.plot(real[point],imag[point], marker='o',markersize = 2 , c='black', linestyle='none')
     point = point + 1
-"""
+
 
 plot.grid()
 plot.show()
