cb316e6c745f917e4007ff90636ff7f522867630
Miles Dawkins <milesdawkins0@gmail.com>
2025-05-17 23:50:49 -0600

    going to look into adding resistors and capacitors as shunt or series elements, also looking into parallel networks

--- rf_amp_design_testing.py
+++ rf_amp_design_testing.py
@@ -8,43 +8,43 @@ import time
 ##############setup functions####################
 start_time = time.time()
 script_directory = os.path.dirname(os.path.abspath(__file__))
-file = os.path.join(script_directory,"BFP840FESD_VCE_2.0V_IC_22mA.s2p")
+file = os.path.join(script_directory,"BFP840ESD_VCE_2.0V_IC_22mA.s2p")
 
 #################calulation functions###########################
 freqs = np.linspace(1E1,12E9,1000)
 gain_req = 16.78724978143363
+fo= 3E9
 
 bjt = mt.system_tools.network(file)
-
 amp_calc = mt.circuit_tools.rf_amplifier(bjt)
+microstrip_ref = mt.t_line_tools.microstrip(50,4.4,1.6E-3,1)
+lamb = microstrip_ref.wavelength(fo)
 
-trans_gain = 10*np.log10(np.interp(10E9,amp_calc.frequencies,amp_calc.max_transducer_gain))
-print(trans_gain)
+max_gain = 10*np.log10(np.interp(fo,amp_calc.frequencies,amp_calc.max_transducer_gain))
+print(max_gain)
 
-source_max_gain = 10*np.log10(np.interp(10E9,amp_calc.frequencies,amp_calc.source_max_gain))
+
+
+source_max_gain = 10*np.log10(np.interp(fo,amp_calc.frequencies,amp_calc.source_max_gain))
 print(source_max_gain)
 
-sc,sr,min_gs= amp_calc.calc_gain_circle("source",source_max_gain-0.05,10E9)
+sc,sr,min_gs= amp_calc.calc_gain_circle("source",source_max_gain-0.05,fo)
+
+gs_stub_l = -1*(1/np.atan2(mt.system_tools.gamma_2_impedance(50,np.abs(min_gs)),50))/(2*np.pi)+0.51 # calculating required open circuit stub length
+gs_phase_l = (np.angle(1/((1/50)+(1/(-1J*50*1/np.tan(2*np.pi*gs_stub_l)))))-np.angle(min_gs))/(2*np.pi)+0.01 #calculating phasing line length using differences of phase
 
 
+shunt_source = mt.t_line_tools.microstrip(50,4.4,1.6E-3,gs_stub_l,freqs_in = freqs, typem="open", shunt_in=True, length_unit="lambda", center_freq = fo)
+phase_source = mt.t_line_tools.microstrip(50,4.4,1.6E-3,gs_phase_l+0.5,freqs_in = freqs, length_unit="lambda", center_freq= fo)
 
 
-
-microstrip_ref = mt.t_line_tools.microstrip(50,4.4,1.6E-3,1)
-lamb = microstrip_ref.wavelength(10E9)
-
-gs_stub_l = (1/np.atan(mt.system_tools.gamma_2_impedance(50,np.abs(min_gs))/50))/(2*np.pi) # calculating required open circuit stub length
-gs_phase_l = (np.angle(1/((1/50)+(1/(-1J*50*1/np.tan(2*np.pi*gs_stub_l)))))-np.angle(min_gs))/(2*np.pi) #calculating phasing line length using differences of phase
+print(gs_stub_l*lamb)
+print(gs_phase_l*lamb+0.5*lamb)
 
 
-shunt_source = mt.t_line_tools.microstrip(50,4.4,1.6E-3,gs_stub_l,freqs_in = freqs, typem="short", shunt_in=True, length_unit="lambda", center_freq = 10E9)
-phase_source = mt.t_line_tools.microstrip(50,4.4,1.6E-3,gs_phase_l,freqs_in = freqs, length_unit="lambda", center_freq= 10E9)
+source_match = shunt_source.network**phase_source.network
 
-
-
-
-
-
+amp = source_match ** bjt
 
 
 
@@ -54,13 +54,18 @@ phase_source = mt.t_line_tools.microstrip(50,4.4,1.6E-3,gs_phase_l,freqs_in = fr
 
 print("--- %s seconds ---" % (time.time() - start_time))
 ##################plotting functions#######################
+#plot.plot(amp_calc.frequencies,10*np.log10(amp_calc.max_transducer_gain))
+plot.plot(amp.frequencies,amp.dbmag[1,1])
+plot.plot(amp.frequencies,amp.dbmag[0,0])
+plot.plot(amp.frequencies,amp.dbmag[1,0])
+plot.plot(amp.frequencies,amp.dbmag[0,1])
+plot.legend(["s22","s11","s21","s12"])
+plot.grid()
+
 smith = mt.plotting_tools.smith_chart()
-
-smith.plot_complex(amp_calc.source_max_gain_gamma)
+smith.plot_complex(amp_calc.source_max_gain_gamma[0:int(len(amp_calc.source_max_gain_gamma))])
+smith.plot_complex(source_match.complex[1,1,0:int(len(source_match.complex[1,1])/2)])
 smith.plot_circle(sc,sr)
-
-
-
 
 #annotate every 1GHz
 """
