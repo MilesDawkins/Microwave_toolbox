4c8b11160f0772705fa653d697fc3bdf66feb9b5
Miles Dawkins BH <miles.dawkins@bluehalo.com>
2025-05-22 16:30:09 -0600

    adding diy calibration standards measured s params for testing

--- microwave_toolbox_testing.py
+++ microwave_toolbox_testing.py
@@ -12,11 +12,3 @@ file1 = os.path.join(script_directory,"BFP840FESD_VCE_2.0V_IC_22mA.s2p")
 file2 = os.path.join(script_directory,"BFCN-1560+___Plus25degC.s2p")
 
 #################calulation functions###########################
-freqs = np.linspace(1E6, 1E9,10000)
-bjt = mt.system_tools.network(file1)
-filter = mt.system_tools.network(file2)
-tl = mt.t_line_tools.microstrip(50,4.4,1.6E-3,0.01,freqs_in = freqs)
-bjt2 = bjt ** tl.network ** filter
-
-plot.plot(bjt2.frequencies,bjt2.dbmag[0,0])
-plot.show()
